338276f744c6b18dd5b5ad573948061e96cdd5d5
manikng <manishsingh_23cs244@dtu.ac.in>
2024-06-04 09:41:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const inputBox = document.getElementById('input-box');
+const taskList = document.getElementById('task-list');
+
+function addTask() {
+    if (inputBox.value === '') {
+        alert('Please enter a task');
+    } else {
+        let li = document.createElement('li');
+        li.innerHTML = inputBox.value;
+        li.classList.add('unchecked');
+        taskList.appendChild(li);
+
+        let cross = document.createElement('span');
+        cross.innerHTML = "\u00D7";
+        li.appendChild(cross);
+
+        li.classList.toggle('unchecked');
+        inputBox.value = '';
+    }
+}
+
+inputBox.addEventListener('keyup', function(keyPressed) {
+    if (keyPressed.keyCode === 13) {
+        addTask();
+    }
+});
+
+taskList.addEventListener('click', function(event) {
+    if (event.target.tagName === 'LI') {
+        event.target.classList.toggle('checked');
+    } else if (event.target.tagName === 'SPAN') {
+        event.target.parentElement.remove();
+    }
+});
